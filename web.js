8f4c8b364da0db2007994aa3def1eaa4c7b8f128
George Mamdouh <georgem159@outlook.com>
2026-09-25 20:38:07 +0300

    java1

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ let username;
 
 document.getElementById("button1").onclick = function () {
     username = document.getElementById("name").value;
-    document.getElementById("h12").textContent = "Hello " + username + " you are offically an astronaut!🚀 ";
+    document.getElementById("h12").textContent = "Hello " + username + "!!! you are offically an astronaut!🚀 ";
 };
